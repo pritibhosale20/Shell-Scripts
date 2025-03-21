a7670b8da715e9ffc3c027c2c6388725522774c2
Priti Bhosale <pritibhosale123123@gmail.com>
2025-03-21 15:24:19 +0530

    Create install_lamp.sh

---
+++ amazon-linux/install_lamp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Updating system packages..."
+yum update -y
+
+echo "Installing Apache Web Server..."
+yum install -y httpd
+
+echo "Start and enable Apache service"
+systemctl start httpd
+systemctl enable httpd
+
+echo "Installing MariaDB..."
+yum install -y mariadb105-server
+
+echo "Start and enable MariaDB service"
+systemctl start mariadb
+systemctl enable mariadb
+
+echo "Install PHP and required modules"
+yum install -y php, php-fpm php-mysqlnd
+
+echo "Start and enable php-fpm"
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
+
+echo "Restarting Apache to load PHP"
+systemctl restart httpd
+
+echo "Setting proper permissions"
+chown -R apache:apache /var/www/html
+chmod -R 755 /var/www/html
+
+echo "Creating a test PHP file"
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
+# Print success message
+echo "LAMP stack installed successfully! You can verify PHP at http://your-server-ip/info.php"
